3e844b200cdd2dfc7d857a572c298f0ae72748aa
AmyAlexandra13 <20198880@itla.edu.do>
2021-04-18 19:33:01 -0400

    2reportes

---
+++ CapaPresentacion/Controllers/ReportesController.cs
@@ -0,0 +1,43 @@
+﻿using CapaNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapaPresentacion.Controllers
+{
+    public class ReportesController : Controller
+    {
+        // GET: Reportes
+        public ActionResult Reportes()
+        {
+            return View();
+        }
+
+        public ActionResult ReporteUsuario()
+        {
+            var emps = EmpleadoCN.ListarUsuario();
+            return View(emps);
+        }
+
+
+        public ActionResult VerUsuarios(int id)
+        {
+            var emps = EmpleadoCN.GetUsuario(id);
+            return View(emps);
+        }
+
+        public ActionResult ReporteDepartamento()
+        {
+            var dptos = DepartamentoCN.ListaDepartamento();
+            return View(dptos);
+        }
+
+        public ActionResult VerDepartamento(int id)
+        {
+            var dptos = DepartamentoCN.GetDepartamento(id);
+            return View(dptos);
+        }
+    }
+}
